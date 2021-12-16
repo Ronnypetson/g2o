a0a27a15af85971f06fa6644b7a43a623458e0cb
Ronnypetson <rsronnypetson4@gmail.com>
2021-12-16 00:04:12 -0300

    Added SE3 vertex class

--- g2o/examples/tutorial_epipolar_slam/tutorial_epipolar_slam.cpp
+++ g2o/examples/tutorial_epipolar_slam/tutorial_epipolar_slam.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "se3.h"
+#include "vertex_se3.h"
 
 #include <Eigen/Core>
 #include <Eigen/Geometry>
@@ -8,7 +9,7 @@ using namespace std;
 using namespace g2o;
 using namespace g2o::tutorial;
 
-int main(){
+void test_se3(){
     SE3 pose(1, 0, 0, 0, 0, 0);
     SE3 pose2(2, 0, 0, 0, 0, 0);
     SE3 pose3(3, 0, 0, 0, 0, 0);
@@ -20,4 +21,14 @@ int main(){
     v = pose3 * v;
     std::cout << v << std::endl << std::endl;
     std::cout << pose3.inverse().toVector() << std::endl << std::endl;
+}
+
+void test_vertex_se3(){
+    VertexSE3 vse3;
+    std::cout << "Testing VertexSE3" << std::endl << std::endl;
+}
+
+int main(){
+    // test_se3();
+    test_vertex_se3();
 }
