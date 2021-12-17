729485e2687192a52b0787c971a009b903dea8ae
Ronnypetson <rsronnypetson4@gmail.com>
2021-12-17 01:17:11 -0300

    Adding simulation class

---
+++ g2o/examples/tutorial_epipolar_slam/simulator.cpp
@@ -0,0 +1,334 @@
+// g2o - General Graph Optimization
+// Copyright (C) 2011 R. Kuemmerle, G. Grisetti, W. Burgard
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the
+//   documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+// IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#include "simulator.h"
+
+#include "g2o/stuff/sampler.h"
+
+#include <map>
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+namespace g2o {
+  namespace tutorial {
+
+    using namespace Eigen;
+
+#  ifdef _MSC_VER
+    inline double round(double number)
+    {
+      return number < 0.0 ? ceil(number - 0.5) : floor(number + 0.5);
+    }
+#  endif
+
+    typedef std::map<int, std::map<int, Simulator::LandmarkPtrVector> > LandmarkGrid;
+
+    Simulator::Simulator()
+    {
+      time_t seed = time(0);
+      Sampler::seedRand(static_cast<unsigned int>(seed));
+    }
+
+    Simulator::~Simulator()
+    {
+    }
+
+    void Simulator::simulate(int numNodes, const SE3& sensorOffset)
+    {
+      // simulate a robot observing landmarks while travelling on a grid
+      int steps = 5;
+      double stepLen = 1.0;
+      int boundArea = 50;
+
+      double maxSensorRangeLandmarks = 2.5 * stepLen;
+
+      int landMarksPerSquareMeter = 1;
+      double observationProb = 0.8;
+
+      int landmarksRange=2;
+
+      Vector3d transNoise(0.05, 0.01, 0.01);
+      Vector3d rotNoise(0.01, 0.01, 0.01);
+      Vector3d landmarkNoise(0.05, 0.05, 0.05);
+
+      Vector2d bound(boundArea, boundArea);
+
+      VectorXd probLimits;
+      probLimits.resize(MO_NUM_ELEMS);
+      for (int i = 0; i < probLimits.size(); ++i)
+        probLimits[i] = (i + 1) / (double) MO_NUM_ELEMS;
+
+      Eigen::Matrix<double, 6, 6> covariance;
+      covariance.fill(0.);
+      /*
+      TODO: Check later.
+      */
+      covariance(0, 0) = transNoise[0] * transNoise[0];
+      covariance(1, 1) = transNoise[1] * transNoise[1];
+      covariance(2, 2) = transNoise[2] * transNoise[2];
+      covariance(3, 3) = rotNoise[0] * rotNoise[0];
+      covariance(4, 4) = rotNoise[1] * rotNoise[1];
+      covariance(5, 5) = rotNoise[2] * rotNoise[2];
+      Eigen::Matrix<double, 6, 6> information = covariance.inverse();
+
+      SE3 maxStepTransf(stepLen * steps, 0, 0, 0, 0, 0);
+      Simulator::PosesVector& poses = _poses;
+      poses.clear();
+      LandmarkVector& landmarks = _landmarks;
+      landmarks.clear();
+      Simulator::GridPose firstPose;
+      firstPose.id = 0;
+      firstPose.truePose = SE3(0, 0, 0, 0, 0, 0);
+      firstPose.simulatorPose = SE3(0, 0, 0, 0, 0, 0);
+      poses.push_back(firstPose);
+      cerr << "Simulator: sampling nodes ...";
+      while ((int)poses.size() < numNodes) {
+        // add straight motions
+        for (int i = 1; i < steps && (int)poses.size() < numNodes; ++i) {
+          Simulator::GridPose nextGridPose = generateNewPose(poses.back(), SE3(stepLen, 0, 0, 0, 0, 0), transNoise, rotNoise);
+          poses.push_back(nextGridPose);
+        }
+        if ((int)poses.size() == numNodes)
+          break;
+
+        // sample a new motion direction
+        double sampleMove = Sampler::uniformRand(0., 1.);
+        int motionDirection = 0;
+        while (probLimits[motionDirection] < sampleMove && motionDirection+1 < MO_NUM_ELEMS) {
+          motionDirection++;
+        }
+
+        SE3 nextMotionStep = getMotion(motionDirection, stepLen);
+        Simulator::GridPose nextGridPose = generateNewPose(poses.back(), nextMotionStep, transNoise, rotNoise);
+
+        // check whether we will walk outside the boundaries in the next iteration
+        SE3 nextStepFinalPose = nextGridPose.truePose * maxStepTransf;
+        if (fabs( (nextStepFinalPose.translation())(0) ) >= bound[0] || fabs( (nextStepFinalPose.translation())(1) ) >= bound[1]) {
+          //cerr << "b";
+          // will be outside boundaries using this
+          for (int i = 0; i < MO_NUM_ELEMS; ++i) {
+            nextMotionStep = getMotion(i, stepLen);
+            nextGridPose = generateNewPose(poses.back(), nextMotionStep, transNoise, rotNoise);
+            nextStepFinalPose = nextGridPose.truePose * maxStepTransf;
+            if (fabs( (nextStepFinalPose.translation())(0) ) < bound[0] && fabs( (nextStepFinalPose.translation())(1) ) < bound[1])
+              break;
+          }
+        }
+
+        poses.push_back(nextGridPose);
+      }
+      cerr << "done." << endl;
+
+      // creating landmarks along the trajectory
+      cerr << "Simulator: Creating landmarks ... ";
+      LandmarkGrid grid;
+      for (PosesVector::const_iterator it = poses.begin(); it != poses.end(); ++it) {
+        int ccx = (int)round(it->truePose.translation()(0));
+        int ccy = (int)round(it->truePose.translation()(1));
+        for (int a=-landmarksRange; a<=landmarksRange; a++)
+          for (int b=-landmarksRange; b<=landmarksRange; b++){
+            int cx=ccx+a;
+            int cy=ccy+b;
+            LandmarkPtrVector& landmarksForCell = grid[cx][cy];
+            if (landmarksForCell.size() == 0) {
+              for (int i = 0; i < landMarksPerSquareMeter; ++i) {
+                Landmark* l = new Landmark();
+                double offx, offy;
+                do {
+                  offx = Sampler::uniformRand(-0.5*stepLen, 0.5*stepLen);
+                  offy = Sampler::uniformRand(-0.5*stepLen, 0.5*stepLen);
+                } while (hypot_sqr(offx, offy) < 0.25*0.25);
+                l->truePose[0] = cx + offx;
+                l->truePose[1] = cy + offy;
+                landmarksForCell.push_back(l);
+              }
+            }
+          }
+      }
+      cerr << "done." << endl;
+
+      cerr << "Simulator: Simulating landmark observations for the poses ... ";
+      double maxSensorSqr = maxSensorRangeLandmarks * maxSensorRangeLandmarks;
+      int globalId = 0;
+      for (PosesVector::iterator it = poses.begin(); it != poses.end(); ++it) {
+        Simulator::GridPose& pv = *it;
+        int cx = (int)round(it->truePose.translation()(0));
+        int cy = (int)round(it->truePose.translation()(1));
+        int numGridCells = (int)(maxSensorRangeLandmarks) + 1;
+
+        pv.id = globalId++;
+        SE3 trueInv = pv.truePose.inverse();
+
+        for (int xx = cx - numGridCells; xx <= cx + numGridCells; ++xx)
+          for (int yy = cy - numGridCells; yy <= cy + numGridCells; ++yy) {
+            LandmarkPtrVector& landmarksForCell = grid[xx][yy];
+            if (landmarksForCell.size() == 0)
+              continue;
+            for (size_t i = 0; i < landmarksForCell.size(); ++i) {
+              Landmark* l = landmarksForCell[i];
+              double dSqr = hypot_sqr(pv.truePose.translation()(0) - l->truePose(0), pv.truePose.translation()(1) - l->truePose(1));
+              if (dSqr > maxSensorSqr)
+                continue;
+              double obs = Sampler::uniformRand(0.0, 1.0);
+              if (obs > observationProb) // we do not see this one...
+                continue;
+              if (l->id < 0)
+                l->id = globalId++;
+              if (l->seenBy.size() == 0) {
+                Vector3d trueObservation = trueInv * l->truePose;
+                Vector3d observation = trueObservation;
+                observation[0] += Sampler::gaussRand(0., landmarkNoise[0]);
+                observation[1] += Sampler::gaussRand(0., landmarkNoise[1]);
+                l->simulatedPose = pv.simulatorPose * observation;
+              }
+              l->seenBy.push_back(pv.id);
+              pv.landmarks.push_back(l);
+            }
+          }
+
+      }
+      cerr << "done." << endl;
+
+      // add the odometry measurements
+      _odometry.clear();
+      cerr << "Simulator: Adding odometry measurements ... ";
+      for (size_t i = 1; i < poses.size(); ++i) {
+        const GridPose& prev = poses[i-1];
+        const GridPose& p = poses[i];
+
+        _odometry.push_back(GridEdge());
+        GridEdge& edge = _odometry.back();
+
+        edge.from = prev.id;
+        edge.to = p.id;
+        edge.trueTransf = prev.truePose.inverse() * p.truePose;
+        edge.simulatorTransf = prev.simulatorPose.inverse() * p.simulatorPose;
+        edge.information = information;
+      }
+      cerr << "done." << endl;
+
+      _landmarks.clear();
+      _landmarkObservations.clear();
+      // add the landmark observations
+      {
+        cerr << "Simulator: add landmark observations ... ";
+        Matrix3d covariance; covariance.fill(0.);
+        covariance(0, 0) = landmarkNoise[0]*landmarkNoise[0];
+        covariance(1, 1) = landmarkNoise[1]*landmarkNoise[1];
+        covariance(2, 2) = landmarkNoise[2]*landmarkNoise[2];
+        Matrix3d information = covariance.inverse();
+
+        for (size_t i = 0; i < poses.size(); ++i) {
+          const GridPose& p = poses[i];
+          for (size_t j = 0; j < p.landmarks.size(); ++j) {
+            Landmark* l = p.landmarks[j];
+            if (l->seenBy.size() > 0 && l->seenBy[0] == p.id) {
+              landmarks.push_back(*l);
+            }
+          }
+        }
+
+        for (size_t i = 0; i < poses.size(); ++i) {
+          const GridPose& p = poses[i];
+          SE3 trueInv = (p.truePose * sensorOffset).inverse();
+          for (size_t j = 0; j < p.landmarks.size(); ++j) {
+            Landmark* l = p.landmarks[j];
+            Vector3d observation;
+            Vector3d trueObservation = trueInv * l->truePose;
+            observation = trueObservation;
+            if (l->seenBy.size() > 0 && l->seenBy[0] == p.id) { // write the initial position of the landmark
+              observation = (p.simulatorPose * sensorOffset).inverse() * l->simulatedPose;
+            } else {
+              // create observation for the LANDMARK using the true positions
+              observation[0] += Sampler::gaussRand(0., landmarkNoise[0]);
+              observation[1] += Sampler::gaussRand(0., landmarkNoise[1]);
+            }
+
+            _landmarkObservations.push_back(LandmarkEdge());
+            LandmarkEdge& le = _landmarkObservations.back();
+
+            le.from = p.id;
+            le.to = l->id;
+            le.trueMeas = trueObservation;
+            le.simulatorMeas = observation;
+            le.information = information;
+          }
+        }
+        cerr << "done." << endl;
+      }
+
+
+      // cleaning up
+      for (LandmarkGrid::iterator it = grid.begin(); it != grid.end(); ++it) {
+        for (std::map<int, Simulator::LandmarkPtrVector>::iterator itt = it->second.begin(); itt != it->second.end(); ++itt) {
+          Simulator::LandmarkPtrVector& landmarks = itt->second;
+          for (size_t i = 0; i < landmarks.size(); ++i)
+            delete landmarks[i];
+        }
+      }
+
+    }
+
+    Simulator::GridPose Simulator::generateNewPose(const Simulator::GridPose& prev, const SE3& trueMotion, const Eigen::Vector3d& transNoise, const Eigen::Vector3d& rotNoise)
+    {
+      Simulator::GridPose nextPose;
+      nextPose.id = prev.id + 1;
+      nextPose.truePose = prev.truePose * trueMotion;
+      SE3 noiseMotion = sampleTransformation(trueMotion, transNoise, rotNoise);
+      nextPose.simulatorPose = prev.simulatorPose * noiseMotion;
+      return nextPose;
+    }
+
+    SE3 Simulator::getMotion(int motionDirection, double stepLen)
+    {
+      switch (motionDirection) {
+        case MO_LEFT:
+          return SE3(stepLen, 0, 0.5*M_PI, 0, 0, 0);
+        case MO_RIGHT:
+          return SE3(stepLen, 0, -0.5*M_PI, 0, 0, 0);
+        default:
+          cerr << "Unknown motion direction" << endl;
+          return SE3(stepLen, 0, -0.5*M_PI, 0, 0, 0);
+      }
+    }
+
+    SE3 Simulator::sampleTransformation(const SE3& trueMotion_, const Eigen::Vector3d& transNoise, const Eigen::Vector3d& rotNoise)
+    {
+      Vector6 trueMotion = trueMotion_.toVector();
+      SE3 noiseMotion(
+          trueMotion[0] + Sampler::gaussRand(0.0, transNoise[0]),
+          trueMotion[1] + Sampler::gaussRand(0.0, transNoise[1]),
+          trueMotion[2] + Sampler::gaussRand(0.0, transNoise[2]),
+          trueMotion[3] + Sampler::gaussRand(0.0, transNoise[3]),
+          trueMotion[4] + Sampler::gaussRand(0.0, transNoise[4]),
+          trueMotion[5] + Sampler::gaussRand(0.0, transNoise[5])
+          );
+      return noiseMotion;
+    }
+
+  } // end namespace
+} // end namespace
---
+++ g2o/examples/tutorial_epipolar_slam/simulator.h
@@ -0,0 +1,125 @@
+// g2o - General Graph Optimization
+// Copyright (C) 2011 R. Kuemmerle, G. Grisetti, W. Burgard
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are
+// met:
+//
+// * Redistributions of source code must retain the above copyright notice,
+//   this list of conditions and the following disclaimer.
+// * Redistributions in binary form must reproduce the above copyright
+//   notice, this list of conditions and the following disclaimer in the
+//   documentation and/or other materials provided with the distribution.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+// IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+// TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+// PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+// HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+// SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+// TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+// PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+// LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+// NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+// SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+#ifndef G2O_SIMULATOR_H
+#define G2O_SIMULATOR_H
+
+#include "se3.h"
+#include "g2o_tutorial_epipolar_slam_api.h"
+
+#include <vector>
+#include <map>
+
+namespace g2o {
+  namespace tutorial {
+
+    class G2O_TUTORIAL_EPIPOLAR_SLAM_API Simulator {
+      public:
+
+        enum G2O_TUTORIAL_EPIPOLAR_SLAM_API MotionType {
+          MO_LEFT, MO_RIGHT,
+          MO_NUM_ELEMS
+        };
+
+        /**
+         * \brief simulated landmark
+         */
+        struct G2O_TUTORIAL_EPIPOLAR_SLAM_API Landmark
+        {
+          EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+          int id;
+          Eigen::Vector3d truePose;
+          Eigen::Vector3d simulatedPose;
+          std::vector<int> seenBy;
+          Landmark() : id(-1) {}
+        };
+        typedef std::vector<Landmark, Eigen::aligned_allocator<Landmark> > LandmarkVector;
+        typedef std::vector<Landmark*> LandmarkPtrVector;
+
+        /**
+         * simulated pose of the robot
+         */
+        struct G2O_TUTORIAL_EPIPOLAR_SLAM_API GridPose
+        {
+          EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+          int id;
+          SE3 truePose;
+          SE3 simulatorPose;
+          LandmarkPtrVector landmarks;      ///< the landmarks observed by this node
+        };
+        typedef std::vector<GridPose, Eigen::aligned_allocator<GridPose> >  PosesVector;
+
+        /**
+         * \brief point association constraint
+         */
+        struct G2O_TUTORIAL_EPIPOLAR_SLAM_API GridEdge
+        {
+          int from;
+          int to;
+          SE3 trueTransf;
+          SE3 simulatorTransf;
+          Eigen::Matrix<double, 6, 6> information;
+          EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+        };
+        typedef std::vector<GridEdge, Eigen::aligned_allocator<GridEdge> >  GridEdgeVector;
+
+        struct G2O_TUTORIAL_EPIPOLAR_SLAM_API LandmarkEdge
+        {
+          int from;
+          int to;
+          Eigen::Vector3d trueMeas;
+          Eigen::Vector3d simulatorMeas;
+          Eigen::Matrix3d information;
+          EIGEN_MAKE_ALIGNED_OPERATOR_NEW;
+        };
+        typedef std::vector<LandmarkEdge, Eigen::aligned_allocator<LandmarkEdge> >  LandmarkEdgeVector;
+
+      public:
+        Simulator();
+        ~Simulator();
+
+        void simulate(int numPoses, const SE3& sensorOffset = SE3());
+
+        const PosesVector& poses() const { return _poses;}
+        const LandmarkVector& landmarks() const { return _landmarks;}
+        const GridEdgeVector& odometry() const { return _odometry;}
+        const LandmarkEdgeVector& landmarkObservations() const { return _landmarkObservations;}
+
+      protected:
+        PosesVector _poses;
+        LandmarkVector _landmarks;
+        GridEdgeVector _odometry;
+        LandmarkEdgeVector _landmarkObservations;
+
+        GridPose generateNewPose(const GridPose& prev, const SE3& trueMotion, const Eigen::Vector3d& transNoise, const Eigen::Vector3d& rotNoise);
+        SE3 getMotion(int motionDirection, double stepLen);
+        SE3 sampleTransformation(const SE3& trueMotion_, const Eigen::Vector3d& transNoise, const Eigen::Vector3d& rotNoise);
+    };
+
+  } // end namespace
+} // end namespace
+
+#endif
